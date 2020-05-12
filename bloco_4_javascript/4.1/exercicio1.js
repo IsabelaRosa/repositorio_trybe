4562231c002e5b0fd72a16b60f5ed72bbe44619c
Isabela Rosa <isabelarosa954@gmail.com>
2020-05-12 18:29:45 -0300

    nova pasta bloco 4

---
+++ bloco_4_javascript/4.1/exercicio1.js
@@ -0,0 +1,29 @@
+let num1 = 17;
+let num2 = 13;
+let resultado;
+
+//PROGRAMA 1: ADIÇÃO
+
+resultado = (num1 + num2);
+console.log(resultado);
+
+//PROGRAMA 2: SUBTRAÇÃO
+
+resultado = (num1 - num2);
+console.log(resultado);
+
+//PROGRAMA 3: MULTIPLICAÇÃO
+
+resultado = (num1 * num2);
+console.log(resultado);
+
+//PROGRAMA 4: DIVISÃO
+
+resultado = (num1 / num2);
+console.log(resultado);
+
+//PROGRAMA 5: MÓDULO
+
+resultado = (num1 % num2);
+console.log(resultado);
+
