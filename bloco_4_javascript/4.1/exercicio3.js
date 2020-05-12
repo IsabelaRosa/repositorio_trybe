fc7f95e4193e260141ac68db215834727219b917
Isabela Rosa <isabelarosa954@gmail.com>
2020-05-12 18:43:27 -0300

    adicionando exercício 3

---
+++ bloco_4_javascript/4.1/exercicio3.js
@@ -0,0 +1,14 @@
+let num1 = 59 ; num2 = 24 ; num3 = 112;
+
+if (num1 > num2 && num1 > num3)
+{
+    console.log(num1);
+}
+else if (num2 > num3)
+{
+    console.log(num2);
+}
+else
+{
+    console.log(num3);
+}
