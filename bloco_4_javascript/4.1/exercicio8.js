2825227f51170649fbfdd028d4d86ed6bb420fac
Isabela Rosa <isabelarosa954@gmail.com>
2020-05-14 12:07:37 -0300

    adicionando o exercício 8

---
+++ bloco_4_javascript/4.1/exercicio8.js
@@ -0,0 +1,10 @@
+let varA = 1; varB = 41; varC = 66;
+let varTeste = false;
+
+if ((varA % 2 == 0) || (varB % 2 == 0) || (varC % 2 == 0))
+{
+  varTeste = true;
+  console.log(varTeste);
+}
+else
+console.log(varTeste);
