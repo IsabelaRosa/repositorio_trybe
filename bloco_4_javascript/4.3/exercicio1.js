abb1156eb9f8cc9627610ea070415d5a121a68dc
Isabela Rosa <isabelarosa954@gmail.com>
2020-05-20 21:54:39 -0300

    criando pasta do dia 4.3 e adicionando exercício 1

---
+++ bloco_4_javascript/4.3/exercicio1.js
@@ -0,0 +1,14 @@
+let entrada = 4;
+
+let linha = [];
+
+for (let i = 0; i < entrada; i+=1)
+{
+  linha += "* ";
+}
+//adicionei um espaço na string para ficar mais bonitinho quando imprimir tudo.
+
+for (let j = 0; j < entrada; j+=1)
+{
+  console.log(linha);
+}
